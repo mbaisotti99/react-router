f1f93482c1cdf943f558d8089be5fb16fcb3eeea
mbaisotti99 <matteo.baisotti@gmail.com>
2025-01-09 17:30:46 +0100

    Contacts WIP

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -7,7 +7,7 @@ function AboutPage() {
             <main className="about">
                 <div className="container abt">
                     <h2>
-                        SE NON AVETE UN MANDATO NON DICO NULLA
+                        SE NON AVETE UN MANDATO NON DICO NULLA!
                     </h2>
                     <p>Inviare mandato a moot42069@mindyourownbusiness.net</p>
                 </div>
--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -6,7 +6,23 @@ function ContactPage() {
             <MainNav />
             <main className="contacts">
                 <div className="container">
-                    <div>aSD</div>
+                    <p className="pgp">
+                    -----BEGIN PGP PUBLIC KEY BLOCK-----
+mDMEZ3BUxxYJKwYBBAHaRw8BAQdA4jK7DLf56J2fwU5Dfa5BSejloabnfqC0Um5U
+NQw5kl+0JHMxY2tCNHNzIDxqZXJyeWZvdHRlcjkyNDNAcHJvdG9uLm1lPoiZBBMW
+CgBBFiEE5whVWZym7ITbMwxpRKrY8VKF1qMFAmdwVMcCGwMFCY0WAjkFCwkIBwIC
+IgIGFQoJCAsCBBYCAwECHgcCF4AACgkQRKrY8VKF1qMWIwEA/WhbyzvSBa3HL9DZ
+egpMCKkhjRvzjJk44li9O72irt0A/3e1Bm3cYJSbPD2otJTSCWuUGO5VerdbkXDU
+x/yphy0NuDgEZ3BUxxIKKwYBBAGXVQEFAQEHQMwbtWvcoECcv2GCCFuwSD1HgH/0
+P7eMf0P/995t7epcAwEIB4h+BBgWCgAmFiEE5whVWZym7ITbMwxpRKrY8VKF1qMF
+AmdwVMcCGwwFCY0WAjkACgkQRKrY8VKF1qNSpQEAyN/D9+Wc0pwpiF8tXbv9BSWR
+Njx2l1KNVg6M+uz+2ssBAL+DjSVpe9UzjwmTu4ceBVsg7hyhLGVyJZ/UGpwevS4O
+=/vMb
+-----END PGP PUBLIC KEY BLOCK-----
+                    </p>
+                    <p>
+                        rightbehindyou@jabber.org
+                    </p>
                 </div>
             </main>
         </>
